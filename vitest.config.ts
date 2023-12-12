76b22e5b79a5f2b04c082a2855cb1039d373311f
{{Greg Leeper}} <{{gregleeper19@gmail.com}}>
2023-12-12 13:44:42 -0600

    Commented out test configuration in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,14 +6,14 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [react()],
 	css: { postcss: { plugins: [] } },
-	test: {
-		include: ['./app/**/*.test.{ts,tsx}'],
-		setupFiles: ['./tests/setup/setup-test-env.ts'],
-		globalSetup: ['./tests/setup/global-setup.ts'],
-		restoreMocks: true,
-		coverage: {
-			include: ['app/**/*.{ts,tsx}'],
-			all: true,
-		},
-	},
+	// test: {
+	// 	include: ['./app/**/*.test.{ts,tsx}'],
+	// 	setupFiles: ['./tests/setup/setup-test-env.ts'],
+	// 	globalSetup: ['./tests/setup/global-setup.ts'],
+	// 	restoreMocks: true,
+	// 	coverage: {
+	// 		include: ['app/**/*.{ts,tsx}'],
+	// 		all: true,
+	// 	},
+	// },
 })
